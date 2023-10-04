4ee94ade6d3f31dc9c8ced89446b2757e9a41356
Cristina Jung <cristijung@gmail.com>
2023-10-04 17:00:44 -0300

    atual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AssistirComponent } from './pages/assistir/assistir.component';
+import { CaracteresComponent } from './pages/caracteres/caracteres.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'assistir', component: AssistirComponent },
+  { path: 'caracteres', component: CaracteresComponent },
+  { path: 'galeria', component: GaleriaComponent},
+  { path: 'contato', component: ContatoComponent},
+  { path: '**', component: NotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
